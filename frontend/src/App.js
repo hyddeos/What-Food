04d7b7ca96342588facff3e330bbeac6019688f2
hyddeos <ericsageby@hotmail.com>
2022-12-16 21:54:14 +0100

    routes, and colortheme added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
+import React from 'react';
+import Home from './pages/Home';
+import Login from './pages/Login';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 export default function App() {
   return (
-    <h1 className="text-1xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <BrowserRouter>
+      <div className='h-screen bg-blue-900'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function App() {
+    return (
+        <section>
+            <h1>test</h1>
+            <div>
+                <p>This is a login</p>
+            </div>
+        </section>
+
+    );
+  }
