7808c040e2ff6d2857b024f8fe56f14f0cc9cfb1
hyddeos <ericsageby@hotmail.com>
2023-01-29 10:03:48 +0100

    first

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import Home from './pages/Home';
 import Login from './pages/Login';
+import HandleDishes from './pages/HandleDishes';
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
@@ -13,23 +14,14 @@ function App() {
   const [loggedIn, setLoggedIn] = React.useState(false);
   const token = Cookies.get('token');
 
-  /*
-  if (token) {
-    // The token cookie exists, so the user is logged in.
-    console.log("user has token")
-  } else {
-    // The token cookie does not exist, so the user is not logged in.
-    console.log("user has not token")
-  }
-  */
-
   return (
     <BrowserRouter>
       <div className=''>
           <Routes>
               <Route path="/login" element={<Login loggedIn={loggedIn} setLoggedIn={setLoggedIn} token={token}  />} />
               <Route path="/" element={<Home loggedIn={loggedIn} setLoggedIn={setLoggedIn} token={token} />} />
-              <Route path="/home" element={<Home Home loggedIn={loggedIn} setLoggedIn={setLoggedIn} token={token}  />} /> 
+              <Route path="/home" element={<Home Home loggedIn={loggedIn} setLoggedIn={setLoggedIn} token={token}  />} />
+              <Route path="/dishes" element={<HandleDishes loggedIn={loggedIn} setLoggedIn={setLoggedIn} token={token}  />} /> 
           </Routes>
       </div>
     </BrowserRouter>
