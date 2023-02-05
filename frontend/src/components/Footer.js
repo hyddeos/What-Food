4eade85fe1956467f4e08028444d9c4c7ba6d8cc
hyddeos <ericsageby@hotmail.com>
2023-02-05 14:52:35 +0100

    css fix footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
 
     return (
         <footer className="bg-stroke sticky top-[99vh]">
-            <p className="text-center text-sec-300">WhatFood. 
+            <p className="text-center text-sec-300">WhatFood, {new Date().getFullYear()}. 
             Made by <a className="font-bold" href="https://eshtropy.se">Esh</a> {new Date().getFullYear()}. 
             See more over at <a className="font-bold" href="https://github.com/hyddeos/What-Food">Github</a>.</p>
         </footer>
